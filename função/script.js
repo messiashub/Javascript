21cd5112146ffa0851fedb753d1b693aec0f3233
messiashub <claudineimessias@hotmail.com>
2020-11-25 00:42:00 -0300

    update

--- função/script.js
+++ função/script.js
@@ -22,3 +22,23 @@ console.log(nome('Nilda','de Souza'));
 let soma = (n,x)=> console.log(`A soma entre ${n} e ${x} é = ${n + x}`);
 
 soma(16,4);
+
+// Outro
+
+
+function media(nota1,nota2){
+    return (nota1 + nota2)/ 2;
+}
+
+
+function passou(media){
+
+    if(media >= 5 ){
+        return 'passou';
+    }else{
+        return 'repetiu';
+    }
+}
+
+/* console.log(media(5, 4)); */
+console.log(passou(media(5,5)));
