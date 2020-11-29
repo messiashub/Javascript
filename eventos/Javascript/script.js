ea07ec7075f0819082b02d473e411f5abcd2d795
messiashub <claudineimessias@hotmail.com>
2020-11-29 18:00:57 -0300

    eventos

---
+++ eventos/Javascript/script.js
@@ -0,0 +1,18 @@
+
+
+let botao = document.getElementById('btn');
+
+botao.addEventListener('click',function(){
+    alert('cliquei');
+});
+
+botao.addEventListener('click',function(){
+    console.log('de novo');
+});
+
+botao.addEventListener('click', function(){
+    botao.style.backgroundColor = 'red';
+})
+    
+
+
