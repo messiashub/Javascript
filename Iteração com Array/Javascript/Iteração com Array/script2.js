6b926e96ecae6295e1c6f199e37dd0349e9d43be
messiashub <claudineimessias@hotmail.com>
2020-12-02 13:25:43 -0300

    update

--- Iteração com Array/Javascript/Iteração com Array/script2.js
+++ Iteração com Array/Javascript/Iteração com Array/script2.js
@@ -1,8 +1,8 @@
 
 // MAP 
+// O método map() invoca a função callback passada por argumento para cada elemento do Array e devolve um novo Array como resultado.
 
-
-
+//1 DOBRAR NUMERO
 let numero = [45,4,9,16,25];
 
 let numero2 = numero.map(dobro);
@@ -14,6 +14,23 @@ function dobro(valor){
 console.log(numero2);
 console.log("--------------------");
 
+// 2 DOBRAR NUMERO COM UMA  FUNÇÃO ANÔNIMA DE CALLBACK;
+let numeros = [2,4,7,25];
+let numerosA = numeros.map(function(valor, index){
+    return valor * 2;
+});
+
+console.log(numerosA);
+console.log("--------------------------------")
+
+// 3 DOBRAR NUMERO USANDO UMA ARROW FUNCTION
+let num= [2,4,5,35];
+let num2 = num.map(value => value * 2);
+
+console.log(num2);
+console.log("--------------------")
+
+// TRANSFORMANDO EM MAIÚSCULAS
 let nomes = ['maria','neia','epaminondas','silvia','michaela'];
 
 let nomes2 = nomes.map(maiuscula);
@@ -34,4 +51,11 @@ let nomeB = nomeA.map(function(valor,indice){
 })
 
 console.log(nomeB);
+console.log("---------------------------------");
+
+// OUTRA FORMA -> USANDO UMA  ARROW FUNCTION
+let lista = ['claudio','kevin','bruno'];
+let novaLista = lista.map(value => value.toUpperCase());
+
+console.log(novaLista);
 
