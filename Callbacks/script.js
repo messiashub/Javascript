8a993a6133b6ee42b5940d74b841e1177ad5c4ce
messiashub <claudineimessias@hotmail.com>
2020-12-07 00:28:18 -0300

    Callbacks

---
+++ Callbacks/script.js
@@ -0,0 +1,35 @@
+// CALLBACKS
+
+
+function soma(a,b, res){
+    let soma = a + b;
+    res(soma)
+}
+
+
+function multi(a,b,res){
+    let multi = a * b;
+    res(multi);
+}
+
+function exibir(num){
+    console.log(num)
+}
+
+soma(2,4,exibir);
+multi(2,4, exibir);
+
+// Outro
+
+function minhaConta(some){
+    console.log(some);
+}
+
+function calcular(a,b, some){
+    let calc = a + b
+    some(calc);
+}
+
+calcular(5,5,minhaConta)
+
+
