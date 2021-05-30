cebbc9c3a5994c59fdcb2934ccf5439eeeb3a352
messiashub <claudineimessias@hotmail.com>
2021-05-30 18:59:39 -0300

    update

---
+++ innerHTML/script.js
@@ -0,0 +1,21 @@
+
+/* innerHTML => PODE-SE OBTER OU MODIFICAR O VALOR DAS PROPRIEDADES
+DE UM ELEMENTO*/
+
+// EX 1:
+
+// MODIFICANDO O VALOR DO ELEMENTO 'input' COM 'value'
+let cx = document.querySelector('#caixa').value='messias'; 
+console.log(cx);
+
+// MODIFICANDO O VALOR DA TAG "P" COM "innerHTML"
+let txt = document.querySelector("#texto").innerHTML ="messias"
+
+// INSERINDO UM VALOR NA TAG "P" COM "innerHTML"
+let num = 51
+
+let rua = document.querySelector(".address").innerHTML=`Rua Bento Teixeira n°${num}`
+
+// OBTENDO O VALOR  DA DIV COM "innerHTML"
+let div = document.querySelector("#dv").innerHTML;
+console.log(div);
