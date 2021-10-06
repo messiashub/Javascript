2da9159af77ccf3ddaa08bed74c7aedef7bcbd97
messiashub <claudineimessias@hotmail.com>
2021-10-06 19:27:09 -0300

    finalizando

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -6,25 +6,63 @@ SetItem: para definir uma "chave:valor" , para armazenar o dado
 GETItem: para obter o valor da chave
 RemoveItem: para remover o elemento armazenado*/
 
-// Verificando se o navegador tem localStorage
 
-if(typeof(Storage)!="underfine"){
-    localStorage.setItem("canal","CFB Cursos");
+// COLETANDO DADOS DO VISITANTE
 
-    let verCanal = localStorage.getItem('canal');
+let vcorF;
+let vcorT;
+let vNome;
+let show = document.querySelector("#fconfig");
 
-    document.write(verCanal);
 
-    console.log(verCanal);
+if(localStorage.nome){
+    vcorF= localStorage.corFundo;
+    vcorT= localStorage.corTexto;
+    vNome = localStorage.nome;
+    defineCor(1,vcorF);
+    defineCor(2,vcorT);
+    document.querySelector("#painel").innerHTML = `Bem vindo ${vNome} <hr>` ;
 
-}else{
-    document.write("Sem suporte a Web Storage")
+   
+}
+ function gravar(){
+    vNome = document.querySelector("#fnome").value;
+    localStorage.setItem("nome", vNome);
+    localStorage.corFundo = vcorF;
+    localStorage.corTexto = vcorT;
+    show.style.display="none";
+    console.log(vNome);
+ }
+
+function defineCor(op,cor){
+    if(op==1){
+        document.body.style.backgroundColor=cor;
+        vcorF = cor;
+        console.log(`Fundo cor:${vcorF}`);
+    }else{
+        document.body.style.color=cor;
+        vcorT = cor;
+        console.log(`Texto cor:${vcorT}`);
+
+    }
+}
+
+function configura(){ 
+    show.style.display="block"
+    console.log("oi")
+
+
 }
 
 
 
 
+// CONTAGEM DE VISITAS NO SISTEMA (contador de acesso)
 
+//lendo o codigo:
+/* localStorage.visitas existe? 
+   se existe = localStorage.visita recebe ele mesmo convertido em numero +1
+   se não , cria com o valor 1 */
 
 
 // 1° Definindo uma chave. 
@@ -34,3 +72,4 @@ if(typeof(Storage)!="underfine"){
 //localStorage.removeItem('canal');
 /* ----------------------------------------------- */
 // 3° Obtendo o valor da chave 
+// Verificando se o navegador tem localStorage
