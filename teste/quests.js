3348409b44077e07f042f81f7321aa3baacfd008
messiashub <claudineimessias@hotmail.com>
2021-10-08 01:01:55 -0300

    update

---
+++ teste/quests.js
@@ -0,0 +1,33 @@
+
+
+function seculo(n){
+    if(n <= 100){
+        return 1;
+    }if(n <= 200){
+        return 2;
+    }
+}
+
+console.log(seculo(101));
+
+let sec = 1;
+let ano = 1
+function seculo2(n){
+    for(let i = 1; i <= n; i++){
+
+    }
+}
+
+
+let convert = function(n){
+    let num = n.toString().slice(2,4);
+    
+    return num;
+}
+
+console.log(convert(1940));
+console.log("----")
+
+/* console.log(convert2(500));
+console.log(convert2(960));
+console.log(convert2(99)); */
