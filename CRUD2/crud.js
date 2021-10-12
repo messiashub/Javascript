166163c49dc9165da97c44ade445148f563b2062
messiashub <claudineimessias@hotmail.com>
2021-10-12 17:53:21 -0300

    função para adicionar

--- CRUD2/crud.js
+++ CRUD2/crud.js
@@ -1,4 +1,4 @@
-// Criando uma classe est
+// Criando uma função factory - ela tem retorno
 function List(){
     let items = [];
     function add(item){   // método para adicionar itens
@@ -29,5 +29,54 @@ function List(){
         updates:updates,
         getItem:getItem,
         getItens:getItens
+    }
+}
+
+// Função construtora
+function ProdutoModel(nome,valor,qtd) {
+    this.nome = nome;
+    this.valor = valor;
+    this.qtd = qtd;
+
+    this.getTotal = function(){
+        return parseInt(this.qtd) * parseFloat(this.valor);
+    }
+}
+
+// Função factory- tem retorno
+
+function ListProduto(){  // vai adicionar os item em um array
+    let list = List();
+
+    function getAll(){
+        return list.getItens(); // vai retornar todos os elementos de List()
+
+    }
+    function submitProduto(){  // vai  interagir com o HTML
+
+        let $index = document.querySelector('#form_index').value;
+        let $nome = document.querySelector('#form_nome').value;
+        let $valor = document.querySelector('#form_valor').value;
+        let $qtd = document.querySelector('#form_qtd').value;
+
+        let $produto = new ProdutoModel($nome,$valor,$qtd)
+        list.add($produto);  // list vai adicionar 
+        resetForm();        // vai limpar os formulários
+    }
+    function resetForm(){    // vai limpar os campos do formulário
+        let $index = document.querySelector('#form_index');
+        let $nome = document.querySelector('#form_nome');
+        let $valor = document.querySelector('#form_valor');
+        let $qtd = document.querySelector('#form_qtd');
+       
+        $index.value="";
+        $nome.value="";
+        $valor.value="";
+        $qtd.value="";
+    }
+    return{
+        getAll:getAll,            // propriedade e valor =  valor é a propria função
+        submitProduto:submitProduto, //        //
+        resetForm: resetForm,
     }
 }
