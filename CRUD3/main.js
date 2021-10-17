d7524b29c412a1921ad0faa1317883a509477c4e
messiashub <claudineimessias@hotmail.com>
2021-10-17 00:48:21 -0300

    update

--- CRUD3/main.js
+++ CRUD3/main.js
@@ -4,10 +4,10 @@ const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
 const closeModal = () => {
-     $limparCampos();
+    $limparCampos();
     document.getElementById('modal').classList.remove('active'); //
 }
-   
+
 
 /* const $clienteTemporario={
     nome:"Nilda", 
@@ -17,45 +17,45 @@ const closeModal = () => {
 } */
 
 
-const $pegueDoLocalStorage = ()=> JSON.parse(localStorage.getItem('db_cliente')) ?? [];
+const $pegueDoLocalStorage = () => JSON.parse(localStorage.getItem('db_cliente')) ?? [];
 
-const $enviarParaLocalStorage = (dbCliente)=> localStorage.setItem("db_cliente",JSON.stringify(dbCliente));
+const $enviarParaLocalStorage = (dbCliente) => localStorage.setItem("db_cliente", JSON.stringify(dbCliente));
 
 // CRUD - create read update delete model
 
 // delete
-const $deleteCliente = (index)=>{
+const $deleteCliente = (index) => {
     const dbCliente = $lerCliente();
-    dbCliente.splice(index,1);  
+    dbCliente.splice(index, 1);
     $enviarParaLocalStorage(dbCliente);
 }
 //update - ataulizar
-const $updateCliente = (index, cliente)=>{ 
+const $updateCliente = (index, cliente) => {
     const dbCliente = $lerCliente();
     dbCliente[index] = cliente;
     $enviarParaLocalStorage(dbCliente);
 }
 // read -ler
-const $lerCliente = ()=> $pegueDoLocalStorage();
+const $lerCliente = () => $pegueDoLocalStorage();
 
 // create
-const $criarCliente =function(cliente){
+const $criarCliente = function (cliente) {
     const dbCliente = $pegueDoLocalStorage()
     dbCliente.push(cliente);
-    $enviarParaLocalStorage(dbCliente)   
+    $enviarParaLocalStorage(dbCliente)
 }
 
-const $campoValido = function(){ 
+const $campoValido = function () {
     return document.querySelector('#form').reportValidity()
 
 }
-const $limparCampos = ()=>{ 
+const $limparCampos = () => {
     const $campos = document.querySelectorAll(".modal-field");
-    $campos.forEach((campo)=> campo.value="")
+    $campos.forEach((campo) => campo.value = "")
 
 }
-const $criarLinha=(cliente,indice)=>{ // 1.1 PEGANDO O INDICE DA LINHA
-    const $novaLinha =document.createElement('tr');
+const $criarLinha = (cliente, indice) => { // 1.1 PEGANDO O INDICE DA LINHA
+    const $novaLinha = document.createElement('tr');
     $novaLinha.innerHTML = `
     <td>${cliente.nome}</td>
     <td>${cliente.email}</td>
@@ -66,56 +66,75 @@ const $criarLinha=(cliente,indice)=>{ // 1.1 PEGANDO O INDICE DA LINHA
         <button type="button" class="button red" id="deletar-${indice}">Excluir</button>
     </td>
     `
-   
+
     document.querySelector("#tabelaCliente>tbody").appendChild($novaLinha);
 }
 
-const $limparTabela = () =>{ 
+const $limparTabela = () => {
     const $linhas = document.querySelectorAll("#tabelaCliente>tbody>tr");
     $linhas.forEach(linha => linha.parentNode.removeChild(linha))
 }
-const $updateTabela = ()=>{
+const $updateTabela = () => {
     const dbCliente = $lerCliente();
-    $limparTabela() 
+    $limparTabela()
     dbCliente.forEach($criarLinha);
 }
-$updateTabela() 
+$updateTabela()
 
-const $salvarCliente = function(){ 
-    if($campoValido()){  
-        const $cliente ={  
-            nome:document.getElementById('nome').value,
-            email:document.getElementById('email').value,
+const $salvarCliente = function () {
+    if ($campoValido()) {
+        const $cliente = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
             celular: document.getElementById('celular').value,
-            cidade : document.getElementById('cidade').value,
+            cidade: document.getElementById('cidade').value,
         }
-        $criarCliente($cliente) 
-        $limparCampos() 
-        $updateTabela() 
-        closeModal()
-        
-    }   
+        const $indice = document.querySelector("#nome").dataset.index;
+        if ($indice == "new") {
+            $criarCliente($cliente)
+            $limparCampos()
+            $updateTabela()
+            closeModal()
+        }else{
+            console.log('editar')
+        }
+
+
+
+
+    }
 }
 
-// 1 EDITAR E  DELETAR CLIENTE
+// EDITAR E  DELETAR CLIENTE
 
-const $editeCliente = function(indice){ // 3 PEGANDO SOMENTE O CLIENTE QUE VAI SER EDITADO
-    const $cliente = $lerCliente()[indice] // 3 COMO $lerCliente é um array, pego somente o "indice"
-    console.log($cliente)
+// 1 PREENCHENDO OS CAMPOS
+const $preencherCampos = function ($cliente) {
+    document.querySelector("#nome").value = $cliente.nome
+    document.querySelector("#email").value = $cliente.email
+    document.querySelector("#celular").value = $cliente.celular
+    document.querySelector("#cidade").value = $cliente.cidade
 
 }
-const $editarDeletar = function(event){
-    if(event.target.type == "button"){  // 1 PEGANDO O ALVO QUE TEM O TIPO "TYPE"
-        const [action, indice]= event.target.id.split("-") // 2
-       /*  console.log(action,indice) */
 
-       if(action == "editar"){ 
+const $editeCliente = function (indice) {
+    const $cliente = $lerCliente()[indice]
+    /* console.log($cliente) */
+    $preencherCampos($cliente); // 1 $preencherCampos recebe o $cliente
+    openModal();//1 QUANDO CLICAR EM $editeCliente vai abrir o modal
+
+}
+const $editarDeletar = function (event) {
+    if (event.target.type == "button") {
+        const [action, indice] = event.target.id.split("-")
+
+
+        if (action == "editar") {
             $editeCliente(indice)
 
-        }else{
+        } else {
             console.log('deletando o cliente')
-        }     
-    }      
+        }
+    }
 }
 $updateTabela()
 
@@ -127,7 +146,7 @@ document.getElementById('modalClose')
     .addEventListener('click', closeModal);
 
 document.querySelector('#salvar')
-    .addEventListener('click', $salvarCliente); 
+    .addEventListener('click', $salvarCliente);
 
 document.querySelector("#tabelaCliente>tbody")
-    .addEventListener('click',$editarDeletar);// 1
+    .addEventListener('click', $editarDeletar);
